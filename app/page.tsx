4ccdf99233499290c17915bd470b1e778e8b6544
Andrey Fadeev <andfadeev@gmail.com>
2023-07-18 00:36:27 +0100

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -2,47 +2,19 @@ import {allPosts} from "@/.contentlayer/generated"
 import Link from "next/link"
 import Image from "next/image";
 import {GithubSocialIcon, LinkedIn} from "@/components/social-links";
+import {AuthorInfo} from "@/components/author-info";
 
 
 export default function Home() {
     const buttonClassName = "px-10 py-5  bg-slate-100 dark:bg-slate-900 rounded-md mt-2 inline-block no-underline hover:underline underline-offset-[3px]"
     return (
         <div>
-            <div className="flex flex-row relative justify-between w-full mt-10 items-center">
-
-                <div>
-                    <Image
-                        src={"/avatar.png"}
-                        alt="Andrey Fadeev photo"
-                        width={120}
-                        height={120}
-                        className="rounded-full"
-                    />
-                </div>
-
-                <div className="flex flex-col relative w-full ml-4">
-                    <h1 className="font-extrabold text-2xl tracking-tight">
-                        Andrey Fadeev
-                    </h1>
-                    <p className="text-gray-600 dark:text-gray-400">
-                        Staff Software Engineer at Kroo Bank
-                    </p>
-                    <div className={"mt-2 flex gap-2 text-gray-600 dark:text-gray-400"}>
-                        <LinkedIn/>
-                        <GithubSocialIcon/>
-                    </div>
-                    {/*<p className="text-gray-600 dark:text-gray-400">*/}
-                    {/*  All good things come to those who wait.*/}
-                    {/*</p>*/}
-                </div>
-            </div>
-
-
+            <AuthorInfo/>
             <div className="prose dark:prose-invert mt-10">
                 <h2>About</h2>
 
                 <p>
-                    I'm interested in functional programming and web development. Currently, work as a backend developer using Clojure as the main
+                    I&apos;m interested in functional programming and web development. Currently, work as a backend developer using Clojure as the main
                     language, but I am also curious about full stack development.
                 </p>
 
